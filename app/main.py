2682dd2a0a35ae2121d835ba89b75a50775474b0
Sahis <sahisneupane23@gmail.com>
2025-03-08 14:32:18 -0800

    feat: Added API key authentication

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from app.utils.api_utility import verify_api_key, get_api_key
+from fastapi import FastAPI, Depends
+from fastapi.security.api_key import APIKeyHeader
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/secure-data")
+def secure_data(api_key: str = Depends(verify_api_key)):
+    return {"message": "You have access to secure data"}

53fcd6cd57dd2a6c708dd51fd0d4802b6cdd97c9
dchang21 <devinchang27@gmail.com>
2025-04-24 00:25:15 -0700

    added reading_grade and from_SA to student table

--- app/models.py
+++ app/models.py
@@ -8,10 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # database credentials
-MYSQL_USER = "apcsa2020"# os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = "csulasuperb_2025"# os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = "47.33.78.253"# os.getenv("MYSQL_HOST")
-MYSQL_DB = "Braille_AI"# os.getenv("MYSQL_DB")
+MYSQL_USER = ""# os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = ""# os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = ""# os.getenv("MYSQL_HOST")
+MYSQL_DB = ""# os.getenv("MYSQL_DB")
 MYSQL_PORT = 3306# os.getenv("MYSQL_PORT")
 
 # database url

bb1d2e41f8d9a36d36a9c4edf1e2c4d28bebd16e
lowees <luisbadillo0826@gmail.com>
2025-08-03 15:10:11 -0700

    fix: update CORS allowed origins for production environment

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://production.d1dfl6c2j787ys.amplifyapp.com/"],
+    allow_origins=["https://production.d28lo1xwcra81p.amplifyapp.com/"],
     allow_methods=["GET", "POST", "PUT"],                   
     allow_headers=["*"],
 )

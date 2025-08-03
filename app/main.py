d5099082b80fd7c194cba811d15a99d4eca5e374
lowees <luisbadillo0826@gmail.com>
2025-08-03 14:14:17 -0700

    Fix: changed CORS settings

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_methods=["*"],                   
+    allow_origins=["https://production.d1dfl6c2j787ys.amplifyapp.com/"],
+    allow_methods=["GET", "POST", "PUT"],                   
     allow_headers=["*"],
 )
 

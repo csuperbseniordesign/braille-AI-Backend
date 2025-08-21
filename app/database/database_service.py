4537fd2a3f066764aa8001687e7428aba25c0e27
lowees <luisbadillo0826@gmail.com>
2025-08-20 19:53:44 -0700

    Fix: removed previous change due to error in comprehension

--- app/database/database_service.py
+++ app/database/database_service.py
@@ -11,7 +11,7 @@ MYSQL_HOST = os.getenv("MYSQL_HOST")
 DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/BrailleAI"
 
 # set up SQLAlchemy
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
